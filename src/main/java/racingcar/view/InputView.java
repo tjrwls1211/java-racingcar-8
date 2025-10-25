dd2e35f22b0188ec846db7e0140125aa226ddc38
tjrwls1211 <seokjin6635@gmail.com>
2025-10-25 23:05:17 +0900

    feat(view): 자동차 이름 검증 로직 구현 - 5자 초과 경우 - 이름이 비어있는 경우

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -9,11 +9,23 @@ public class InputView {
     public static List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
-        return Arrays.asList(input.split(","));
+        List<String> carNames = Arrays.asList(input.split(","));
+        carNames.forEach(InputView::validateCarName);
+        return carNames;
     }
 
     public static int readAttemptCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
+    }
+
+    public static void validateCarName(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+        }
+
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
     }
 }
