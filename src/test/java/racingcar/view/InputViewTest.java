dee8257c14a91f65a12e83de7c25ed70865b1006
tjrwls1211 <seokjin6635@gmail.com>
2025-10-25 22:45:13 +0900

    test(view): 자동차 이름 검증 테스트 추가 - 5자 초과 경우 - 이름이 비어있는 경우

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class InputViewTest extends NsTest {
@@ -19,6 +20,20 @@ class InputViewTest extends NsTest {
         run("pobi,woni", "5");
 
         assertThat(output()).contains("시도할 횟수는 몇 회인가요?");
+    }
+
+    @Test
+    void 자동차_이름이_5자를_초과하면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputView.validateCarName("abcdefg"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름이_비어있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> InputView.validateCarName(" "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 비어있을 수 없습니다.");
     }
 
     @Override
